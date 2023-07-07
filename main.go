39467b10322e4f42dbc93a13e77594cb86801a79
Spyup <jason88tu@gmail.com>
2023-07-07 21:24:56 +0800

    add cros && final v2

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	Routers "ddic/routers"
 	Routines "ddic/routine"
 	"fmt"
+
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -12,6 +14,12 @@ func main() {
 
 	// Run routine
 	Routines.Run()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://shihyan.nmg.cs.thu.edu.tw:8081"},
+		AllowMethods: []string{"PUT", "POST", "GET", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"X-Requested-With", "Content-Type"},
+	}))
 
 	// Run server
 	errorMessage := router.Run(":9000")
